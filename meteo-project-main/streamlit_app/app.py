056b1d4ab1d82d5bb62ed083786e5bcabaab1fb2
Kipfab <115087981+Kipfab@users.noreply.github.com>
2022-10-25 17:54:53 +0200

    app.py modifs

--- meteo-project-main/streamlit_app/app.py
+++ meteo-project-main/streamlit_app/app.py
@@ -15,7 +15,8 @@ st.set_page_config(
     page_icon="https://datascientest.com/wp-content/uploads/2020/03/cropped-favicon-datascientest-1-32x32.png",
 )
 
-with open("style.css", "r") as f:
+cssPathRoot = '../streamlit_app/'
+with open(cssPathRoot + "style.css", "r") as f:
     style = f.read()
 
 st.markdown(f"<style>{style}</style>", unsafe_allow_html=True)
